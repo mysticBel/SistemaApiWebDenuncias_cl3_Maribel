fbe8c12c64e05347e4d38a95c01cd3a7b0c4f56d
mysticBel <dnxbel@gmail.com>
2023-08-20 01:48:29 -0500

    Agregar archivos de proyecto.

---
+++ SistemaApiWebDenuncias_cl3_Maribel/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace SistemaApiWebDenuncias_cl3_Maribel
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
